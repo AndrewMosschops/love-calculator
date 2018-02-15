6427e01c5ec550c1690e3ac25eb337208b7014ee
AndrewMosschops <andrewmosschops@yahoo.co.uk>
2018-02-15 20:03:52 +0000

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+//properties in class to keep data in the template
+
+  name1 = "";
+  name2 = "";
+  
+  get score() {
+    const letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    for (let i = 0; i < letters.length; i++) {
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
+  }
+
+}
